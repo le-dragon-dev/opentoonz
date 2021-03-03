e3be5ad80ab15a4431764061ab76c36cc6e5e8fe
Le-dragon-dev <david.garcia.pro13@gmail.com>
2021-03-03 08:56:59 +0100

    Add the codec too

--- toonz/sources/image/tzl/tiio_tzl_structure.cpp
+++ toonz/sources/image/tzl/tiio_tzl_structure.cpp
@@ -1,18 +1,53 @@
 #include "tiio_tzl_structure.h"
 
 // ****************************************************************************
+// TLVFileHeader: Constructor
+// ****************************************************************************
+TLVFileHeader::TLVFileHeader(const QString& magicWord, const QString& creator,
+                             __int32 hdrSize, __int32 width, __int32 height,
+                             __int32 framecount, __int32 frameTableOffset,
+                             __int32 iconTableOffset, const QString& codec):
+    hdrSize(hdrSize), levelWidth(width), levelHeight(height), framecount(framecount),
+    frameTableOffset(frameTableOffset), iconTableOffset(iconTableOffset)
+{
+  assert(magicWord.length() >= TLV_MAGIC_WORD_SIZE);
+  assert(creator.length() <= TLV_CREATOR_SIZE);
+  assert(width > 0);
+  assert(height > 0);
+  assert(framecount > 0 && framecount < 60000);
+  assert(frameTableOffset > sizeof(TLVFileHeader));
+  assert(iconTableOffset > sizeof(TLVFileHeader));
+  assert(codec.length() == TLV_CODEC_SIZE);
+
+  memcpy(this->magicWord, magicWord.toStdString().c_str(), TLV_MAGIC_WORD_SIZE);
+  memcpy(this->creator, creator.toStdString().c_str(), creator.length());
+  memcpy(this->codec, codec.toStdString().c_str(), TLV_CODEC_SIZE);
+
+  // At the moment, only this magic word and codec are accepted
+  assert(std::strncmp(this->magicWord, "TLV14", 5ull) == 0);
+  assert(std::strncmp(this->codec, "LZ0 ", 4ull) == 0);
+}
+
+// ****************************************************************************
 // TLVFileHeader: Control the validity of:
-// - Magic word
+// - Magic word & codec
 // - Level width and height
 // - Frame count
 // - Offset tables
 // ****************************************************************************
 bool TLVFileHeader::isValid() const {
-  return std::strncmp(magicWord, "TLV14", 5) == 0 && // Magic word TLV14
-         levelWidth > 0 && levelHeight > 0        && // Width & height > 0
-         framecount > 0 && framecount < 60000     && // Framecount [1;59999]
-         frameTableOffset > sizeof(TLVFileHeader) && // Offset > header size
-         iconTableOffset > sizeof(TLVFileHeader);    // Offset > header size
+  // Magic word should be TLV14 (at the moment)
+  const bool is_magic_valid = std::strncmp(magicWord, "TLV14", 5) == 0;
+
+  const bool is_values_valid = levelWidth > 0 && levelHeight > 0        &&
+                               framecount > 0 && framecount < 60000     &&
+                               frameTableOffset > sizeof(TLVFileHeader) &&
+                               iconTableOffset > sizeof(TLVFileHeader);
+
+  // Codec should be LZ0 (at the moment)
+  const bool is_codec_valid = std::strncmp(codec, "LZ0 ", 4ull) == 0;
+
+  return is_magic_valid && is_values_valid && is_codec_valid;
 }
 
 // ****************************************************************************
@@ -34,12 +69,15 @@ std::ostream& operator<<(std::ostream& os, const TLVFileHeader& header) {
 // TLVFileHeader: Read a header from stream
 // ****************************************************************************
 std::istream& operator>>(std::istream& is, TLVFileHeader& header) {
+  char codec[TLV_CODEC_SIZE] {};
+
   is >> header.magicWord
      >> header.creator
      >> header.hdrSize
      >> header.levelWidth >> header.levelHeight
      >> header.framecount
-     >> header.frameTableOffset >> header.iconTableOffset;
+     >> header.frameTableOffset >> header.iconTableOffset
+     >> header.codec;
 
   // Asserts
   assert(std::strncmp(header.magicWord, "TLV14", 5ull) == 0);
@@ -48,6 +86,7 @@ std::istream& operator>>(std::istream& is, TLVFileHeader& header) {
   assert(header.framecount > 0 && header.framecount < 60000);
   assert(header.frameTableOffset > sizeof(TLVFileHeader));
   assert(header.iconTableOffset > sizeof(TLVFileHeader));
+  assert(std::strncmp(header.codec, "LZ0 ", 4ull) == 0);
 
   return is;
 }
--- toonz/sources/image/tzl/tiio_tzl_structure.h
+++ toonz/sources/image/tzl/tiio_tzl_structure.h
@@ -3,16 +3,18 @@
 #include <QString>
 #include "tnztypes.h"
 
-// Constants
-const size_t TLV_MAGIC_WORD_SIZE = 8ull;
-const size_t TLV_CREATOR_SIZE    = 40ull;
-const size_t TLV_CODEC_SIZE      = 4ull;
-
-const char TLV_CODEC[TLV_CODEC_SIZE] {'L', 'Z', '0', ' '};
+// Size of types
+const size_t TLV_MAGIC_WORD_SIZE { 8 };
+const size_t TLV_CREATOR_SIZE    { 40 };
+const size_t TLV_CODEC_SIZE      { 4 };
 
 // Types
 using TLVMagicWord = char[TLV_MAGIC_WORD_SIZE];
-using TLVCreator = char[TLV_CREATOR_SIZE];
+using TLVCreator   = char[TLV_CREATOR_SIZE];
+using TLVCodec     = char[TLV_CODEC_SIZE];
+
+// Constants
+const TLVCodec TLV_CODEC {'L', 'Z', '0', ' '};
 
 // ****************************************************************************
 // Toonz Raster Level file header
@@ -30,13 +32,13 @@ struct TLVFileHeader {
   TINT32 frameTableOffset {};
   TINT32 iconTableOffset {};
 
+  TLVCodec codec {};
+
   // Constructor
   TLVFileHeader(const QString& magicWord, const QString& creator,
                 TINT32 hdrSize, TINT32 width, TINT32 height, TINT32 framecount,
-                TINT32 frameTableOffset, TINT32 iconTableOffset):
-  hdrSize(hdrSize), levelWidth(width), levelHeight(height), framecount(framecount),
-  frameTableOffset(frameTableOffset), iconTableOffset(iconTableOffset)
-  {}
+                TINT32 frameTableOffset, TINT32 iconTableOffset,
+                const QString& codec = TLV_CODEC);
 
   // Control the validity
   bool isValid() const;
